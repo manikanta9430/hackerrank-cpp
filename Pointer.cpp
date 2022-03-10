27b37018facdf036737ff7c2cabd3d91eb034ccf
Manikanta Rayala <72917946+manikanta9430@users.noreply.github.com>
2022-03-10 23:07:46 +0530

    Create Pointer.cpp

---
+++ Pointer.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    // Complete this function  
+    int tem = *a;
+    *a=*a+*b;
+    if(tem>*b){
+        *b=tem-*b;
+    }
+    else{
+        *b=*b-tem;
+    }
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
